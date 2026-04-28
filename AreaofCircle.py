ac7926937a24f7ac89f9db6cf3397a634d56f980
Aditya Narayan Sharma <37697073+Adinarayanreloaded@users.noreply.github.com>
2026-04-28 18:08:24 +0530

    Delete AreaofCircle.py

--- AreaofCircle.py
+++
@@ -1,7 +0,0 @@
-def calculate_circle_area(radius):
-    # Define the value of pi
-    pi = 3.14159
-    # Calculate the area using the formula for the area of a circle
-    area = pi * radius * radius
-    # Return the calculated area
-    return area
